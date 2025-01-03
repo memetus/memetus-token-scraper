fe2e6bc5798bdc522f75b61832052c29ea69fab8
Resister-boy <hassanpumped17@gmail.com>
2025-01-04 02:51:41 +0900

    feat: add report generater by static json

--- libs/report_by_json.py
+++ libs/report_by_json.py
@@ -1,28 +1,26 @@
-import warnings
+import json
+import sys
 import os
 from dotenv import load_dotenv
 from typing import Annotated, TypedDict, List, Dict, Sequence
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
 from langchain_core.messages import BaseMessage
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
 from pydantic import  Field, create_model
-from openai import OpenAI
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import PromptTemplate
+from openai import OpenAI
+from langchain_core.messages import HumanMessage, AIMessage
 from docx import Document
-from langchain_core.messages import HumanMessage
-from langchain_community.tools.tavily_search import TavilySearchResults
 
 load_dotenv()
 
-warnings.filterwarnings("ignore") 
-os.environ['OPENAI_API_KEY'] = os.getenv("OPENAI_API_KEY")
-os.environ['TAVILY_API_KEY'] = os.getenv("TAVILY_API_KEY")
+os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
+
+topic = input("Enter the topic: ")
 
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
-TAVILY_API_KEY = os.environ["TAVILY_API_KEY"]
 
 class ReportGenerator(TypedDict):
   messages: Annotated[Sequence[BaseMessage], add_messages]
@@ -32,7 +30,14 @@ class ReportGenerator(TypedDict):
   total_sections: int
   full_report: List[Dict[str, str]]
 
+class Article(TypedDict):
+  title: str
+  type: str
+  url: str
+
 graph_builder = StateGraph(ReportGenerator)
+
+llm = ChatOpenAI(model='gpt-4o-2024-08-06')
 
 def create_outline_model(section_count: int):
   fields = {f"section{i}": (str, Field(description=f"Title for section {i}")) for i in range(1, section_count + 1)}
@@ -42,9 +47,7 @@ fields = {f"section{i}": (str, Field(description=f"Title for section {i}")) for 
 
 DynamicOutline = create_model("DynamicOutline", **fields)
 
-DynamicOutline = create_outline_model(3)
-
-llm = ChatOpenAI(model='gpt-4o-2024-08-06')
+DynamicOutline = create_outline_model(20)
 
 def outline_generator(state: ReportGenerator):
   DynamicOutline = create_outline_model(state["total_sections"])
@@ -68,21 +71,23 @@ def outline_generator(state: ReportGenerator):
   })
   return {"outline": outline}
 
-
-search = TavilySearchResults(max_results=3)
-
 client = OpenAI()
 
-def contents_writer(state: ReportGenerator):
+def content_writer(state: ReportGenerator):
+  file_path = 'data.json'
+  search_results = None
+  
+  with open(file_path, "r", encoding="utf-8") as file:
+    search_results = json.load(file)
+
   if "error" in state:
     return {"messages": [AIMessage(content=f"An error occurred: {state['error']}")]}
   
   if state["current_section"] > state["total_sections"]:
     return {"messages": [AIMessage(content="Report completed.")]}
-      
+  
   current_section_key = f"section{state['current_section']}"
   current_topic = state["outline"][current_section_key]
-  search_results = search.invoke(current_topic)    
   
   previous_sections_content = []
   for i in range(1, state['current_section']):
@@ -93,16 +98,15 @@ def contents_writer(state: ReportGenerator):
       {state['outline'][section_key]}
       {state['section_content'][section_key]}
       """)
-
-  print(search_results)
   
   previous_sections = "\n\n".join(previous_sections_content)
   
-  section_prompt = PromptTemplate(
-      template="""
-      Write a detailed section for the topic: {topic}. 
-      
-      Use the following search results for information: {search_results}
+  prompt = PromptTemplate(
+    template="""
+      Your role is write a professional and detailed report on the following topic: {topic}.
+      You should understand well about the {topic}.
+
+      Use the following search results to gather information: {search_results}
 
       It must be a detailed section with statistics and information.
       Also, It always related with Crypto Currency and Blockchain.
@@ -113,48 +117,40 @@ def contents_writer(state: ReportGenerator):
       Write only the content for this section, 
       do not include any image prompts or suggestions.
       Detailed statistics or information is needed, 
-      so you should include collected information from search result.""",
-      input_variables=["topic", "search_results", "previous_sections"],
+      so you should include collected information from search result
+
+      This report will use for educational research for memecoin experted ai model.
+    """,
+    input_variables=["topic", "search_results", "previous_sections"]
   )
-  section_content = llm.invoke(section_prompt.format(
-      topic=current_topic,
-      search_results=search_results,
-      previous_sections=previous_sections
+  section_content = llm.invoke(prompt.format(
+    topic=topic,
+    search_results=search_results,
+    previous_sections=previous_sections
   ))
 
   return {
-      "section_content": section_content.content,
-      "current_section": state["current_section"] + 1,
-      "full_report": state["full_report"] + [{
-          "title": current_topic,
-          "content": section_content.content
-      }]
+    "section_content": section_content.content,
+    "current_section": state["current_section"] + 1,
+    "full_report": state["full_report"] + [{
+        "title": current_topic,
+        "content": section_content.content
+    }]
   }
 
 def report_generator(state: ReportGenerator):
+  file_path = 'data/report.docx'
+
   doc = Document()
   doc.add_heading(f"Report: {state['messages'][0].content}", 0)
 
   for section in state['full_report']:
-      doc.add_heading(section['title'], level=1)
-      doc.add_paragraph(section['content'])
-      doc.add_page_break()
+    doc.add_heading(section['title'], level=1)
+    doc.add_paragraph(section['content'])
+    doc.add_page_break()
 
-  filename = f"report_{state['messages'][0].content}.docx".replace(" ", "_")
-  doc.save(filename)
+  doc.save(file_path)
 
-  return {
-    "messages": [AIMessage(content=f"Report finalized and saved as {filename}.")],
-    "report_file": filename
-  }
-
-graph_builder.add_node('outline_generator', outline_generator)
-graph_builder.add_node('contents_writer', contents_writer)
-graph_builder.add_node('report_generator', report_generator)
-
-graph_builder.add_edge(START, 'outline_generator')
-graph_builder.add_edge('outline_generator', 'contents_writer')
-graph_builder.add_edge('report_generator', END)
 
 def should_continue_writing(state: ReportGenerator):
   if state["current_section"] <= state["total_sections"]:
@@ -163,31 +159,50 @@ def should_continue_writing(state: ReportGenerator):
       return "finalize_report"
 
 graph_builder.add_conditional_edges(
-    "contents_writer",
+    "content_writer",
     should_continue_writing,
     {
-      "write_section": "contents_writer",
+      "write_section": "content_writer",
       "finalize_report": "report_generator"
     }
 )
-graph = graph_builder.compile()
+
 
 current_directory = os.getcwd()
 
-print(f"Current working path: {current_directory}")
+def main(argv):
+  if (len(argv) < 3):
+    sys.exit(1)
 
-topic = input("Write a topic of generating report: ")
+  file_path = argv[1]
+  total_sections = argv[2]
+  try:
+      graph_builder.add_node('outline_generator', outline_generator)
+      graph_builder.add_node('content_writer', content_writer)
+      graph_builder.add_node('report_generator', report_generator)
 
-total_sections = int(input("Write section count: "))
+      graph_builder.add_edge(START, 'outline_generator')
+      graph_builder.add_edge('outline_generator', 'content_writer')
+      graph_builder.add_edge('report_generator', END)
+      
+      graph = graph_builder.compile()
 
-initial_state: ReportGenerator = {
-  "messages": [HumanMessage(content=topic)],
-  "total_sections": total_sections,
-  "current_section": 1,
-  "full_report": [],
-}
+      initial_state: ReportGenerator = {
+        "messages": [HumanMessage(content=topic)],
+        "total_sections": int(total_sections),
+        "current_section": 1,
+        "full_report": [],
+      }
 
-for chunk in graph.stream(initial_state,stream_mode="update"):
-    print(chunk)
+      for chunk in graph.stream(initial_state,stream_mode="update"):
+        print(chunk)
 
-print("\n=== Your report successfully generated ===")
+  except FileNotFoundError:
+    print(f"Error: {file_path} not found.")
+    sys.exit(1)
+  except json.JSONDecodeError as e:
+    print(f"Error: {e}")
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main(sys.argv)
