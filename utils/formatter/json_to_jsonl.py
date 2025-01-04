d736a4a70ee3227e94c3b56d4d9b4f4af27d8a57
Resister-boy <hassanpumped17@gmail.com>
2025-01-05 01:13:18 +0900

    feat: add json_to_jsonl for sentiment

--- utils/formatter/json_to_jsonl.py
+++ utils/formatter/json_to_jsonl.py
@@ -10,6 +10,20 @@ def format_json_to_jsonl(input_files: List[str]) -> None:
         json.dump(entry, outfile)  
         outfile.write("\n")  
 
+def format_json_to_jsonl_sentiment(input_files: List[str]) -> None:
+  for input_file in input_files:
+    output_file = input_file.replace('.json', '.jsonl')
+    with open(input_file, 'r', encoding='utf-8') as infile:
+      data = json.load(infile)  
+      print(data)
+      json_data = []
+
+      for row in data:
+        json_data.append({ 'message': [{ 'role': 'user', 'content': row['score']}, { "role": 'assistant', 'content': row['text']}]})
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        json.dump(json_data, outfile)  
+        outfile.write("\n")
+
 if __name__ == "__main__":
   argv = sys.argv[1:]
-  format_json_to_jsonl(argv)
+  format_json_to_jsonl_sentiment(argv)
